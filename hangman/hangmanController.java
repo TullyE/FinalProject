a53d4e03073aa23a9bbe35bb4cb2858801177699
Tully Eva <TullyEva@icloud.com>
2021-07-07 15:12:46 -0700

    updated the formatting

--- hangman/hangmanController.java
+++ hangman/hangmanController.java
@@ -8,84 +8,84 @@ Tully Eva
 */
 public class hangmanController
 {
-    private hangmanModel theModel;
-    private hangmanView theView;
-    /**
-     *allow the contorller to use the view/model
-     * @param _theView
-     * @param _theModel
-     */
-    public hangmanController(hangmanView _theView, hangmanModel _theModel)
-    {
-        theModel = _theModel;
-        theView = _theView;
-    }
-    /**
-     * hangman game logic
-     */
-    public void logic()
-    {
-        theModel.setWord("./hangman/words.txt");
-        theView.initializeHashMap(theModel.getWord());
-        boolean playing = true;
-        String guess = "";
-        theView.updateKnownLettersView(theModel.getKnownWord());
-        boolean waitingForSubmit = false, gettingGuess = true;
-        
-        while (playing)
-        {
-            theView.setLivesV(theModel.getLives());
-            while (gettingGuess == true)
+   private hangmanModel theModel;
+   private hangmanView theView;
+   /**
+    *allow the contorller to use the view/model
+    * @param _theView
+    * @param _theModel
+    */
+   public hangmanController(hangmanView _theView, hangmanModel _theModel)
+   {
+      theModel = _theModel;
+      theView = _theView;
+   }
+   /**
+    * hangman game logic
+    */
+   public void logic()
+   {
+      theModel.setWord("./hangman/words.txt");
+      theView.initializeHashMap(theModel.getWord());
+      boolean playing = true;
+      String guess = "";
+      theView.updateKnownLettersView(theModel.getKnownWord());
+      boolean waitingForSubmit = false, gettingGuess = true;
+      
+      while (playing)
+      {
+         theView.setLivesV(theModel.getLives());
+         while (gettingGuess == true)
+         {
+            //System.out.println("2");
+            guess = theView.getGuess();
+            theView.invalidate();
+            theView.validate();
+            theView.repaint();
+            waitingForSubmit = theView.submit();
+            if ((waitingForSubmit == true) && (theView.guessedAlready(guess) == false))
             {
-                //System.out.println("2");
-                guess = theView.getGuess();
-                theView.invalidate();
-                theView.validate();
-                theView.repaint();
-                waitingForSubmit = theView.submit();
-                if ((waitingForSubmit == true) && (theView.guessedAlready(guess) == false))
-                {
-                    break;
-                }
-                
+               break;
             }
-            theView.resetGuess();
-            if (theModel.checkGuess(guess))
-            {
-                theModel.updateKnownLetters(guess);
-                theView.updateKnownLettersView(theModel.getKnownWord());
-            }
-            gettingGuess = true;
-            waitingForSubmit = false;
-            if ((theModel.getLives() == 0) || theModel.isWin())
-            {
-                break;
-            }
-        }
-        theView.setLivesV(theModel.getLives());
-        if ((theModel.getLives() == 0))
-        {
-            theView.setLoss(true);
-            try 
-            {
+            
+         }
+         theView.resetGuess();
+         if (theModel.checkGuess(guess))
+         {
+            theModel.updateKnownLetters(guess);
+            theView.updateKnownLettersView(theModel.getKnownWord());
+         }
+         gettingGuess = true;
+         waitingForSubmit = false;
+         if ((theModel.getLives() == 0) || theModel.isWin())
+         {
+            break;
+         }
+      }
+      theView.setLivesV(theModel.getLives());
+      if ((theModel.getLives() == 0))
+      {
+         theView.setLoss(true);
+         try 
+         {
             Thread.sleep(3000);
-            }
-            catch (InterruptedException ex)
-            {
-            }
-            theView.end();
-        }
-        if (theModel.isWin())
-        {
-            theView.setWin(true);
-            try 
-            {
+         }
+         catch (InterruptedException ex)
+         {
+         }
+         theView.end();
+      }
+      if (theModel.isWin())
+      {
+         theView.setWin(true);
+         try 
+         {
             Thread.sleep(3000);
-            }
-            catch (InterruptedException ex)
-            {
-            }
-            theView.end();
-        }
-    }
+         }
+         catch (InterruptedException ex)
+         {
+         }
+         theView.end();
+      }
+   }
 }
--- hangman/hangmanView.java
+++ hangman/hangmanView.java
@@ -14,36 +14,36 @@ import java.awt.event.*;
 import java.util.HashMap;
 public class hangmanView extends JPanel implements KeyListener, MouseListener
 {
-    private JFrame myFrame;
-    private String userGuess = "";
-    private String guessedLetters = "";
-    private String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"};
-    private HashMap<Integer, String> keys = new HashMap<>();
-    private int mouseX = 0;
-    private int mouseY = 0;
-    private String knownLettersView = "";
-    private String theWord;
-    private boolean loss = false;
-    private boolean win = false;
-    private int livesV;
+   private JFrame myFrame;
+   private String userGuess = "";
+   private String guessedLetters = "";
+   private String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"};
+   private HashMap<Integer, String> keys = new HashMap<>();
+   private int mouseX = 0;
+   private int mouseY = 0;
+   private String knownLettersView = "";
+   private String theWord;
+   private boolean loss = false;
+   private boolean win = false;
+   private int livesV;
     
     /**
      * initialize the JFrame
      */
-    public hangmanView()
-    {
-        myFrame = new JFrame("Hangman");
-        myFrame.add(this);
-        myFrame.setSize(new Dimension(517,540));
-        myFrame.setPreferredSize(new Dimension(517,540));
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setVisible(true);
+   public hangmanView()
+   {
+      myFrame = new JFrame("Hangman");
+      myFrame.add(this);
+      myFrame.setSize(new Dimension(517,540));
+      myFrame.setPreferredSize(new Dimension(517,540));
+      myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      myFrame.setVisible(true);
         //Key Input
-        myFrame.setFocusable(true);
-        myFrame.requestFocusInWindow();
-        myFrame.addKeyListener(this);
-        myFrame.addMouseListener(this);
-    }
+      myFrame.setFocusable(true);
+      myFrame.requestFocusInWindow();
+      myFrame.addKeyListener(this);
+      myFrame.addMouseListener(this);
+   }
    /**
     * 
     */
@@ -59,7 +59,7 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
       int key = e.getKeyCode();
       if (keys.containsKey(key))
       {
-          userGuess = keys.get(key);
+         userGuess = keys.get(key);
       }
       // *****************************************************
    }
@@ -80,52 +80,52 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
     /**
     * @param e
     */
-    public void mouseExited(MouseEvent e)
-    {
+   public void mouseExited(MouseEvent e)
+   {
     
-    }
+   }
     /**
      * @param e
      */
-    public void mouseEntered(MouseEvent e)
-    {
+   public void mouseEntered(MouseEvent e)
+   {
     
-    }
+   }
     /**
      * set the X/Y values for the mouse click
      * @param e
      */
-    public void mousePressed(MouseEvent e)
-    {
+   public void mousePressed(MouseEvent e)
+   {
         //System.out.println("Mouse Clicked");
        // Example of getting the x location of the mouse
-       this.mouseX = e.getX();
-       this.mouseY = e.getY();
-    }
+      this.mouseX = e.getX();
+      this.mouseY = e.getY();
+   }
     /**
      * @param e
      */
-    public void mouseReleased(MouseEvent e)
-    {
+   public void mouseReleased(MouseEvent e)
+   {
     
-    }
+   }
     /**
      * @param e
      */
-    public void mouseClicked(MouseEvent e)
-    {
+   public void mouseClicked(MouseEvent e)
+   {
     
-    }
+   }
     /**
      * create the dictionary AND get the View to know the word
      */
    public void initializeHashMap(String word)
    {
-       for(int i = 65; i <= 90; i += 1)
-       {
-           keys.put(i, letters[i-65]);
-       }
-       this.theWord = word;
+      for(int i = 65; i <= 90; i += 1)
+      {
+         keys.put(i, letters[i-65]);
+      }
+      this.theWord = word;
    }
    /**
     * detects an update in the mouseX/mouseY and if
@@ -137,17 +137,17 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
    {
        //System.out.println(this.mouseX);
        //System.out.println(userGuess);
-       if ((this.mouseY > 368) && (this.mouseY < 402) && (!userGuess.equals("")) && (this.mouseX > 219) && (this.mouseX < 439))
-       {
+      if ((this.mouseY > 368) && (this.mouseY < 402) && (!userGuess.equals("")) && (this.mouseX > 219) && (this.mouseX < 439))
+      {
             //System.out.println("submitted");
-            mouseX = 0;
-            mouseY = 0;
-            return true;
-       }
+         mouseX = 0;
+         mouseY = 0;
+         return true;
+      }
        
-       mouseX = 0;
-       mouseY = 0;
-       return false;
+      mouseX = 0;
+      mouseY = 0;
+      return false;
    }
    /**
     * Print the known letters to the screen
@@ -158,14 +158,14 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
    public void showKnown(Graphics2D g2, String known)
    {
         //g2.setFont(new Font("COCOGOOSE", Font.PLAIN, 40));
-        g2.setFont(new Font("Monospaced", Font.PLAIN, 40));
-        g2.setColor(new Color(0, 0, 0));
-        String printThis = "";
-        for(int i = 0; i < known.length(); i += 1)
-        {
-            printThis = printThis + known.substring(i, i+1) + " ";
-        }
-        g2.drawString(printThis, 34, 468);
+      g2.setFont(new Font("Monospaced", Font.PLAIN, 40));
+      g2.setColor(new Color(0, 0, 0));
+      String printThis = "";
+      for(int i = 0; i < known.length(); i += 1)
+      {
+         printThis = printThis + known.substring(i, i+1) + " ";
+      }
+      g2.drawString(printThis, 34, 468);
    }
    /**
     * set the background color
@@ -186,19 +186,19 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
     */
    public String getGuess()
    {
-       return userGuess;
+      return userGuess;
    }
    /**
     * set's the guess to an empty string
     */
    public void resetGuess()
    {
-       if (theWord.indexOf(userGuess) == -1)
-       {
-            guessedLetters = guessedLetters + userGuess + " ";
-       }
+      if (theWord.indexOf(userGuess) == -1)
+      {
+         guessedLetters = guessedLetters + userGuess + " ";
+      }
         
-       userGuess = "";
+      userGuess = "";
    }
    /**
     * paint the objects to the JFrame
@@ -207,44 +207,44 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
    { 
        //hangmanSubmitGuess.png
        //System.out.println("printing");
-       Graphics2D g2 = (Graphics2D) g;
-       createBackgroundColor(g2);
-        drawMan(g2);
-
-       Toolkit t = Toolkit.getDefaultToolkit();
-       Image x = t.getImage("./Images/hangmanSubmitGuess.png");
-       g2.drawImage(x, 0, 0, this);
+      Graphics2D g2 = (Graphics2D) g;
+      createBackgroundColor(g2);
+      drawMan(g2);
+   
+      Toolkit t = Toolkit.getDefaultToolkit();
+      Image x = t.getImage("./Images/hangmanSubmitGuess.png");
+      g2.drawImage(x, 0, 0, this);
        //how can I pass in the Model board without having to create a view board aswell?
        
-
-
-       if (loss)
-       {
-            Toolkit z = Toolkit.getDefaultToolkit();
-            Image loseScreen = z.getImage("./Images/hangmanLoss.png");
-            g2.drawImage(loseScreen, 0, 0, this);
-            showKnown(g2, theWord);
-       }
-       else if (win)
-       {
-            Toolkit z = Toolkit.getDefaultToolkit();
-            Image winScreen = z.getImage("./Images/hangmanWin.png");
-            g2.drawImage(winScreen, 0, 0, this);
-            showKnown(g2, knownLettersView);
+   
+   
+      if (loss)
+      {
+         Toolkit z = Toolkit.getDefaultToolkit();
+         Image loseScreen = z.getImage("./Images/hangmanLoss.png");
+         g2.drawImage(loseScreen, 0, 0, this);
+         showKnown(g2, theWord);
+      }
+      else if (win)
+      {
+         Toolkit z = Toolkit.getDefaultToolkit();
+         Image winScreen = z.getImage("./Images/hangmanWin.png");
+         g2.drawImage(winScreen, 0, 0, this);
+         showKnown(g2, knownLettersView);
             
-       }
-       else
-       {
-        g2.setFont(new Font("Monospaced", Font.PLAIN, 40));
-        g2.setColor(new Color(0, 0, 0));
-        g2.drawString(userGuess, 310, 308);
-
-        g2.setFont(new Font("Monospaced", Font.PLAIN, 40));
-        g2.setColor(new Color(0, 0, 0));
-        g2.drawString(guessedLetters, 174, 210);
-
-        showKnown(g2, knownLettersView);
-       }
+      }
+      else
+      {
+         g2.setFont(new Font("Monospaced", Font.PLAIN, 40));
+         g2.setColor(new Color(0, 0, 0));
+         g2.drawString(userGuess, 310, 308);
+      
+         g2.setFont(new Font("Monospaced", Font.PLAIN, 40));
+         g2.setColor(new Color(0, 0, 0));
+         g2.drawString(guessedLetters, 174, 210);
+      
+         showKnown(g2, knownLettersView);
+      }
    }
    /**
     * get the known letters from the model and then set the known letters view to that string
@@ -252,7 +252,7 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
     */
    public void updateKnownLettersView(String knownLettersModel)
    {
-       this.knownLettersView = knownLettersModel;
+      this.knownLettersView = knownLettersModel;
    }
    /**
     * if the user is out of lives
@@ -261,7 +261,7 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
     */
    public void setLoss(boolean n)
    {
-       this.loss = n;
+      this.loss = n;
    }
    /**
     * if the user guesses the word
@@ -270,7 +270,7 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
     */
    public void setWin(boolean n)
    {
-       this.win = n;
+      this.win = n;
    }
    /**
     * close the window
@@ -288,11 +288,11 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
    public boolean guessedAlready(String guess)
    {
        
-       if (guessedLetters.indexOf(guess) == -1 && knownLettersView.indexOf(guess) == -1)
-       {
-           return false;
-       }
-       return true;
+      if (guessedLetters.indexOf(guess) == -1 && knownLettersView.indexOf(guess) == -1)
+      {
+         return false;
+      }
+      return true;
    }
    /**
     * draws the actual guy
@@ -300,46 +300,46 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
     */
    public void drawMan(Graphics2D g2)
    {
-       if (livesV == 0)
-       {
-        Toolkit t = Toolkit.getDefaultToolkit();
-        Image o = t.getImage("./Images/6Limb.png");
-        g2.drawImage(o, 0, 0, this);
-       }
-       else if (livesV == 1)
-       {
-        Toolkit t = Toolkit.getDefaultToolkit();
-        Image o = t.getImage("./Images/5Limb.png");
-        g2.drawImage(o, 0, 0, this);
-       }
-       else if (livesV == 2)
-       {
-        Toolkit t = Toolkit.getDefaultToolkit();
-        Image o = t.getImage("./Images/4Limb.png");
-        g2.drawImage(o, 0, 0, this);
-       }
-       else if (livesV == 3)
-       {
-        Toolkit t = Toolkit.getDefaultToolkit();
-        Image o = t.getImage("./Images/3Limb.png");
-        g2.drawImage(o, 0, 0, this);
-       }
-       else if (livesV == 4)
-       {
-        Toolkit t = Toolkit.getDefaultToolkit();
-        Image o = t.getImage("./Images/2Limb.png");
-        g2.drawImage(o, 0, 0, this);
-       }
-       else if (livesV == 5)
-       {
-        Toolkit t = Toolkit.getDefaultToolkit();
-        Image o = t.getImage("./Images/1Limb.png");
-        g2.drawImage(o, 0, 0, this);
-       }
-       else if (livesV == 6)
-       {
-
-       }
+      if (livesV == 0)
+      {
+         Toolkit t = Toolkit.getDefaultToolkit();
+         Image o = t.getImage("./Images/6Limb.png");
+         g2.drawImage(o, 0, 0, this);
+      }
+      else if (livesV == 1)
+      {
+         Toolkit t = Toolkit.getDefaultToolkit();
+         Image o = t.getImage("./Images/5Limb.png");
+         g2.drawImage(o, 0, 0, this);
+      }
+      else if (livesV == 2)
+      {
+         Toolkit t = Toolkit.getDefaultToolkit();
+         Image o = t.getImage("./Images/4Limb.png");
+         g2.drawImage(o, 0, 0, this);
+      }
+      else if (livesV == 3)
+      {
+         Toolkit t = Toolkit.getDefaultToolkit();
+         Image o = t.getImage("./Images/3Limb.png");
+         g2.drawImage(o, 0, 0, this);
+      }
+      else if (livesV == 4)
+      {
+         Toolkit t = Toolkit.getDefaultToolkit();
+         Image o = t.getImage("./Images/2Limb.png");
+         g2.drawImage(o, 0, 0, this);
+      }
+      else if (livesV == 5)
+      {
+         Toolkit t = Toolkit.getDefaultToolkit();
+         Image o = t.getImage("./Images/1Limb.png");
+         g2.drawImage(o, 0, 0, this);
+      }
+      else if (livesV == 6)
+      {
+      
+      }
    }
    /**
     * get the number of lives from the model
@@ -347,6 +347,6 @@ public class hangmanView extends JPanel implements KeyListener, MouseListener
     */
    public void setLivesV(int n)
    {
-       livesV = n;
+      livesV = n;
    }
 }
